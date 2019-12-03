a346988bb451cd66d76340aaa545f2368c79c344
Yann GESLIN <ygeslin@e3r12p9.42.fr>
2019-12-03 18:28:36 +0100

    18h18_04122019

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ygeslin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 14:21:58 by ygeslin           #+#    #+#             */
-/*   Updated: 2019/12/03 17:24:46 by ygeslin          ###   ########.fr       */
+/*   Updated: 2019/12/03 18:27:44 by ygeslin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ char		*ft_strjoin_endl(char *s1, const char *s2)
 	char	*str;
 
 	len1 = (s1 == NULL ? 0 : ft_strlen(s1));
-	len2 = (ft_int_strchr(s2, '%') == -1 ? 
+	len2 = (ft_int_strchr(s2, '%') == -1 ?
 	ft_strlen(s2) : ft_int_strchr(s2, '%'));
 	i = 0;
 	if (!(str = malloc(sizeof(char) * (len1 + len2 + 1))))
@@ -135,6 +135,12 @@ void		ft_get_type(t_printf *s)
 	return ;
 }
 
+/*
+*******************************************************************************
+**									DISPLAY
+*******************************************************************************
+*/
+
 void		ft_c(t_printf *s)
 {
 	char tmp[2];
@@ -168,8 +174,8 @@ void		ft_p(t_printf *s)
 
 void		ft_d(t_printf *s)
 {
-	int d;
-	char *tmp;
+	int		d;
+	char	*tmp;
 
 	d = (int)va_arg(s->par, int);
 	tmp = ft_itoa_base(d, "0123456789");
@@ -180,8 +186,8 @@ void		ft_d(t_printf *s)
 
 void		ft_i(t_printf *s)
 {
-	int i;
-	char *tmp;
+	int		i;
+	char	*tmp;
 
 	i = (int)va_arg(s->par, int);
 	tmp = ft_itoa_base(i, "0123456789");
@@ -192,8 +198,8 @@ void		ft_i(t_printf *s)
 
 void		ft_u(t_printf *s)
 {
-	unsigned int u;
-	char *tmp;
+	unsigned int	u;
+	char			*tmp;
 
 	u = (unsigned int)va_arg(s->par, unsigned int);
 	tmp = ft_itoa_base(u, "0123456789");
@@ -226,6 +232,62 @@ void		ft_x_up(t_printf *s)
 	return ;
 }
 
+/*
+*******************************************************************************
+**									DISPLAY
+*******************************************************************************
+*/
+
+void		ft_init_flags(t_printf *s)
+{
+	s->minus = 0;
+	s->positive = 0;
+	s->zero = 0;
+	s->space = 0;
+	s->sharp = 0;
+	s->star = 0;
+	s->width = 0;
+	return ;
+}
+
+int		ft_char_is_flag(t_printf *s)
+{
+	char	*flags;
+	int		i;
+
+	flags = "-+0 #*";
+	i = 0;
+	while (i < 6)
+	{
+		if (s->fmt[0] == flags[i])
+			return (1);
+		i++;	
+	}
+	return (0);
+}
+
+void		ft_get_flags(t_printf *s)
+{
+	ft_init_flags(s);
+	while (ft_char_is_flag(s) > 0)
+	{
+		if(s->fmt[0] == '-')
+			s->minus = 1;
+		if(s->fmt[0] == '+')
+			s->positive = 1;
+		if(s->fmt[0] == '0')
+			s->zero = 1;
+		if(s->fmt[0] == ' ')
+			s->space = 1;
+		if(s->fmt[0] == '#')
+			s->sharp = 1;
+		if(s->fmt[0] == '*')
+			s->star = 1;
+		s->fmt += 1;
+	}
+	return ;
+}
+
 int			ft_printf(const char *format, ...)
 {
 	t_printf	s;
@@ -238,10 +300,10 @@ int			ft_printf(const char *format, ...)
 	{
 		s.str = ft_strjoin_endl(s.str, s.fmt);
 		s.fmt += ft_int_strchr(s.fmt, '%') + 1;
-//		ft_get_flags();
-//		ft_get_width();
-//		ft_get_precision();
-//		ft_get_size();
+		ft_get_flags(&s);
+//		ft_get_width(&s);
+//		ft_get_precision(&s);
+//		ft_get_size(&s);
 		ft_get_type(&s);
 	}
 	ft_putstr_fd(s.str, 1);
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ygeslin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 14:15:52 by ygeslin           #+#    #+#             */
-/*   Updated: 2019/12/03 16:51:17 by ygeslin          ###   ########.fr       */
+/*   Updated: 2019/12/03 18:25:24 by ygeslin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,22 +72,22 @@ int main(void)
 //	int len;
 
 	ft_printf("%c", c);
-	ft_printf("\n%s", s);
-	ft_printf("\n%p", p);
-	ft_printf("\n%d", d);
-	ft_printf("\n%i", i);
-	ft_printf("\n%u", u);
-	ft_printf("\n%x", x);
-	ft_printf("\n%X", X);
+	ft_printf("\n%-9s", s);
+	ft_printf("\n%-9p", p);
+	ft_printf("\n%-9d", d);
+	ft_printf("\n%-9i", i);
+	ft_printf("\n%-9u", u);
+	ft_printf("\n%-9x", x);
+	ft_printf("\n%-9X", X);
 	printf("\n----------------------------------------\n");
-	printf("%c", c);
-	printf("\n%s", s);
-	printf("\n%p", p);
-	printf("\n%+-10d",d);
-	printf("\n%+-10i", i);
-	printf("\n%-10u", u);
-	printf("\n%-10x", x);
-	printf("\n%-10X", X);
+	printf("%-9c", c);
+	printf("\n%-9s", s);
+	printf("\n%-9p", p);
+	printf("\n%-9d",d);
+	printf("\n%-9i", i);
+	printf("\n%-9u", u);
+	printf("\n%-9x", x);
+	printf("\n%-9X", X);
 
 /*
 	printf("c = %c\n", c);
