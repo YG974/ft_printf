63c0d62728f8df572bffae55395013dbdedaee13
Yann GESLIN <ygeslin@e2r8p14.42.fr>
2020-02-12 19:28:21 +0100

    354

--- ft_padding.c
+++ ft_padding.c
@@ -6,45 +6,40 @@
 /*   By: ygeslin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/11 14:21:46 by ygeslin           #+#    #+#             */
-/*   Updated: 2020/02/12 18:19:38 by ygeslin          ###   ########.fr       */
+/*   Updated: 2020/02/12 19:27:32 by ygeslin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
 void		ft_spadding(t_printf *s)
 {
-	char *pad;
-	int		l;
+	char	*pad;
 
-	l = ft_strlen(s->tmp);
-	if (s->precision > l || s->zero == 1)
-		ft_padding_precision(s);
 	s->tmp_len = (ft_strlen(s->tmp) > s->width ? s->width : ft_strlen(s->tmp));
 	if (!(pad = (char *)ft_calloc((s->width - s->tmp_len + 1), sizeof(char))))
 		return ;
-	if ((s->width > 0))
+	if ((s->width > 0 && s->width > s->precision && s->precision >= 0) || s->wstar == 1)
 		pad = ft_memset(pad, ' ', s->width - s->tmp_len);
-	if ((s->minus == 1 || s->wstar == 1))
+	if ((s->minus == 1  || s->neg_width == 1 ))
 		s->tmp = ft_strjoin(s->tmp, pad);
 	else
 		s->tmp = ft_strjoin(pad, s->tmp);
 	s->str = ft_strjoin(s->str, s->tmp);
 	return ;
-
-
 }
 
 void		ft_sprecision(t_printf *s)
 {
 	char *pad;
 
-	ft_get_precision(s);
 	if (!(pad = (char *)ft_calloc(1, sizeof(char))))
 		return ;
 	if (s->precision < 0)
-		s->precision = 0;
-	if (s->pdot == 1)
+	{
+		s->precision = 1;
+		s->pstar = 1;
+	}
+	if ((s->pdot == 1 || s->dot == 1 || s->pstar) && s->neg_precision == 0)
 		s->tmp = ft_strjoin_n(pad, s->tmp, s->precision);
 	return ;
 }
