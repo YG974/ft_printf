09905d5f8c55fda0cb6af10668e6754a189da9f1
Yann Geslin <yanngeslin@LE-MAC.local>
2020-02-15 20:24:11 +0100

    ok

--- ft_write.c
+++ ft_write.c
@@ -1,44 +1,50 @@
-void		ft_order(t_printf s*)
+#include <stdio.h>
+#include "ft_printf.h"
+
+void		ft_order(t_printf *s)
 {
 	s->tmp_len = (s->precision > ft_strlen(s->tmp) ? 
-								s->precision : ft_strlen(s->tmp)) 
-	if()
-	{
+								s->precision : ft_strlen(s->tmp));
+//	if()
+	//{
 		ft_write_width(s);
-		ft_write_sign(s);
+//		ft_write_sign(s);
 		ft_write_preci(s);
 		ft_write_arg(s);
-	}
+/*	}
 	else
 	{
 		ft_write_preci(s);
 		ft_write_sign(s);
 		ft_write_arg(s);
 		ft_write_width(s);
-	}
+	}*/
+	free(s->tmp);
 	return ;
 }
-void		ft_wirte_sign(t_printf s*)
+void		ft_wirte_sign(t_printf *s)
 {
 	if (s->sign == 1)
 	{
-		write(1, '-', 1);
-		s->michel += ft_strlen(s->tmp);
+		write(1, "-", 1);
 		s->michel++; 
 	}
-	else if (s->plus == 1 && s->sign == 0)
+	else if (s->positive == 1 && s->sign == 0)
 	{
-		write(1, '+', 1);
+		write(1, "+", 1);
 		s->michel++; 
 	}
 	return ;
 }
 
-void		ft_write_arg(t_printf s*)
+void		ft_write_arg(t_printf *s)
 {
+		char c;
+
+		c = 0;
 		if (s->null == 1)
 		{
-			write(1, '\0', 1);
+			write(1, &c, 1);
 			s->michel++;
 		}
 		else
@@ -49,23 +55,20 @@ void		ft_write_arg(t_printf s*)
 		return;
 }
 
-void		ft_write_width(t_printf s*)
+void		ft_write_width(t_printf *s)
 {
 	char	c;
 
-	if ((s->zero == 1 || s-pstar == 1))
-		c = '0';
-	else
-		c = ' ';
-	if (s->width > s->precision > s->tmp_len)
+	c = ' ';
+	if (s->width > s->tmp_len)
 	{
 		write(1, &c, s->width - s->tmp_len);
-		s->michel += s->width;
+		s->michel += s->width - s->tmp_len;
 	}
 	return ;
 }
 
-void		ft_write_preci(t_printf s*)
+void		ft_write_preci(t_printf *s)
 {
 	char	c;
 
@@ -73,9 +76,10 @@ void		ft_write_preci(t_printf s*)
 		c = '0';
 	else
 		c = ' ';
-	if (s->width > s->precision > tmp_len)
+	if (s->precision > s->tmp_len)
 	{
-		write(1, &c, s->width - s->tmp_len);
+		write(1, &c, s->precision - s->tmp_len);
+		s->michel += s->precision - s->tmp_len;
 	}
 	return ;
 }
