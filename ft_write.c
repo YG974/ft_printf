cb52bb5847010863366e67471be69a5b6440aab3
Yann GESLIN <ygeslin@e2r12p9.42.fr>
2020-02-19 15:04:37 +0100

    ok_strings

--- ft_write.c
+++ ft_write.c
@@ -6,7 +6,7 @@
 /*   By: ygeslin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 18:06:11 by ygeslin           #+#    #+#             */
-/*   Updated: 2020/02/19 14:35:08 by ygeslin          ###   ########.fr       */
+/*   Updated: 2020/02/19 15:03:36 by ygeslin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -390,5 +390,80 @@ void		ft_write_preci_str(t_printf *s)
 	}
 	else if (s->precision >= s->tmp_len)
 		ft_write_arg(s);
+	return ;
+}
+
+void		ft_print_s(t_printf *s)
+{
+	ft_write_width_str(s);
+	ft_write_arg(s);
+	return ;
+}
+
+void		ft_print_s2(t_printf *s)
+{
+	ft_write_width_str2(s);
+	ft_write_preci_str(s);
+	return ;
+}
+void		ft_print_s3(t_printf *s)
+{
+	ft_write_preci_str(s);
+	ft_write_width_str(s);
+	return ;
+}
+
+void		ft_print_s4(t_printf *s)
+{
+	ft_write_arg(s);
+	ft_write_width_str(s);
+	return ;
+}
+
+void		ft_print_s5(t_printf *s)
+{
+	ft_write_width_str(s);
+	ft_write_arg(s);
+	return ;
+}
+
+void		ft_print_s6(t_printf *s)
+{
+	write(1, " ", 1);
+	s->michel++;
+	return ;
+}
+
+void		ft_print_s7(t_printf *s)
+{
+	ft_write_preci_str(s);
+	ft_write_width_str2(s);
+	return ;
+}
+
+void		ft_s3(t_printf *s)
+{
+	if (s->dot == 1 && s->precision_on == 0)
+		ft_print_s6(s);
+	if (s->minus == 1)
+	{
+		if (s->precision == 0)
+			ft_write_width_str3(s);
+		else if (s->neg_width == 1 && s->neg_precision == 0)
+			ft_print_s3(s);
+		else if (s->neg_width == 0 && s->neg_precision == 0
+				&& s->width <= s->precision)
+			ft_print_s3(s);
+		else if (s->neg_precision == 1)
+			ft_print_s4(s);
+		else if (s->neg_width == 0 && s->neg_precision == 0 &&
+				s->width > s->precision)
+			ft_print_s7(s);
+		else
+		{
+			ft_write_width_str(s);
+			ft_write_preci_str(s);
+		}
+	}
 	return ;
 }
